9af35d0b017dbc6729d637066b11323754baaa17
Arturo Gonzalez <a23050004@unisierra.edu.mx>
2026-03-05 04:29:06 -0700

    Adding Main Branch

---
+++ script.js
@@ -0,0 +1,294 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["palo"];
+
+const musicaFondo= new Audio("assets/Crimson_Valor_s_Crescendo.mp3");
+musicaFondo.loop = true;
+musicaFondo.volume = 0.5;
+let musicaIniciada = false;
+
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+const weapons = [
+  { name: 'palo', power: 5 },
+  { name: 'daga', power: 30 },
+  { name: 'martillo de garras', power: 50 },
+  { name: 'espada', power: 100 },
+  { name: 'hacha', power: 125 },
+  { name: 'maza', power: 190 },
+  { name: 'espada mágica', power: 265 }
+];
+
+const monsters = [
+  { name: "baba", level: 2, health: 15 },
+  { name: "bestia con colmillos", level: 8, health: 95 },
+  { name: "dragón", level: 20, health: 800 }
+];
+
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Ir a la tienda", "Ir a la cueva", "Luchar contra el dragón"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "Estás en la plaza del pueblo. Ves un letrero que dice \"Tienda\".",
+    image: "assets/plaza.png"
+  },
+  {
+    name: "store",
+    "button text": ["Comprar 10 de salud (10 oro)", "Comprar arma (30 oro)", "Ir a la plaza"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "Entras a la tienda.",
+    image: "assets/tienda.png"
+  },
+  {
+    name: "cave",
+    "button text": ["Luchar contra baba", "Luchar contra bestia", "Ir a la plaza"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "Entras a la cueva. Ves algunos monstruos.",
+    image: "assets/cueva.png"
+  },
+  {
+    name: "fight",
+    "button text": ["Atacar", "Esquivar", "Huir"],
+    "button functions": [attack, dodge, goTown],
+    text: "Estás luchando contra un monstruo.",
+    image: "assets/pelea.png"
+  },
+  {
+    name: "kill monster",
+    "button text": ["Ir a la plaza", "Ir a la plaza", "Ir a la plaza"],
+    "button functions": [goTown, goTown, easterEgg],
+    text: 'El monstruo grita "¡Arg!" mientras muere. Ganas puntos de experiencia y encuentras oro.',
+    image: "assets/ganada.png"
+  },
+  {
+    name: "lose",
+    "button text": ["¿VOLVER A JUGAR?", "¿VOLVER A JUGAR?", "¿VOLVER A JUGAR?"],
+    "button functions": [restart, restart, restart],
+    text: "Has muerto. &#x2620;",
+    image: "assets/derrota.png"
+  },
+  { 
+    name: "win", 
+    "button text": ["¿VOLVER A JUGAR?", "¿VOLVER A JUGAR?", "¿VOLVER A JUGAR?"], 
+    "button functions": [restart, restart, restart], 
+    text: "¡Derrotaste al dragón! ¡HAS GANADO EL JUEGO! &#x1F389;",
+    image: "assets/victoria.png"
+  },
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "¿Ir a la plaza?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "Encuentras un juego secreto. Elige un número arriba. Se elegirán diez números al azar entre 0 y 10. ¡Si el número que eliges coincide con uno de los números al azar, ganas!",
+    image: "assets/easteregg.png"
+  }
+];
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update(location) {
+  monsterStats.style.display = "none";
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerHTML = location.text;
+  
+  if (musicaIniciada) {
+      musicaFondo.play();
+  }
+  document.body.style.backgroundImage = "url('" + location.image + "')"; 
+}
+
+function goTown() { update(locations[0]); }
+function goStore() { 
+  update(locations[1]); 
+  if (currentWeapon < weapons.length - 1) {
+    let price = 30 * currentWeapon + 30;
+    button2.innerText = "Comprar arma (" + price + " oro)";
+  } else {
+    button2.innerText = "Vender arma por 15 de oro";
+    button2.onclick = sellWeapon;
+  }
+}
+function goCave() { update(locations[2]); }
+
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "No tienes suficiente oro para comprar salud.";
+  }
+}
+
+function buyWeapon() {
+  if (currentWeapon < weapons.length - 1) {
+    let price = 30 * currentWeapon + 30;
+    
+    if (gold >= price) {
+      gold -= price;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "Ahora tienes un(a) " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " En tu inventario tienes: " + inventory.join(", ");
+      
+      if (currentWeapon < weapons.length - 1) {
+        let nextPrice = 30 * currentWeapon + 30;
+        button2.innerText = "Comprar arma (" + nextPrice + " oro)";
+      } else {
+        button2.innerText = "Vender arma por 15 de oro";
+        button2.onclick = sellWeapon;
+      }
+      
+    } else {
+      text.innerText = "No tienes suficiente oro para comprar un arma.";
+    }
+  } else {
+    text.innerText = "¡Ya tienes el arma más poderosa!";
+    button2.innerText = "Vender arma por 15 de oro";
+    button2.onclick = sellWeapon;
+  }
+}
+
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let soldWeapon = inventory.shift();
+    text.innerText = "Vendiste un(a) " + soldWeapon + ".";
+    text.innerText += " En tu inventario tienes: " + inventory.join(", ");
+  } else {
+    text.innerText = "¡No vendas tu única arma!";
+  }
+}
+
+function fightSlime() { fighting = 0; goFight(); }
+function fightBeast() { fighting = 1; goFight(); }
+function fightDragon() { fighting = 2; goFight(); }
+
+function goFight() {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+}
+
+function attack() {
+  text.innerText = "El/La " + monsters[fighting].name + " ataca.";
+  text.innerText += " Lo atacas con tu " + weapons[currentWeapon].name + ".";
+  health -= getMonsterAttackValue(monsters[fighting].level);
+  if (isMonsterHit()) {
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;    
+  } else {
+    text.innerText += " Fallaste.";
+  }
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    if (fighting === 2) {
+      winGame();
+    } else {
+      defeatMonster();
+    }
+  }
+  if (Math.random() <= .1 && inventory.length !== 1) {
+    text.innerText += " Tu " + inventory.pop() + " se rompió.";
+    currentWeapon--;
+  }
+}
+
+function getMonsterAttackValue(level) {
+  const hit = (level * 5) - (Math.floor(Math.random() * xp));
+  return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+  return Math.random() > .2 || health < 20;
+}
+
+function dodge() {
+  text.innerText = "Esquivaste el ataque de " + monsters[fighting].name + ".";
+}
+
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+
+function lose() { update(locations[5]); }
+function winGame() { update(locations[6]); }
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ["palo"];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
+
+function easterEgg() { update(locations[7]); }
+function pickTwo() { pick(2); }
+function pickEight() { pick(8); }
+
+function pick(guess) {
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "Elegiste " + guess + ". Aquí están los números al azar:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += "¡Correcto! ¡Ganas 20 de oro!";
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += "¡Incorrecto! ¡Pierdes 10 de salud!";
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) { lose(); }
+  }
+}
+
+function iniciarJuego() {
+    musicaFondo.play().then(() => {
+        musicaIniciada = true;
+    }).catch(error => console.log("Error de audio:", error));
+
+    document.querySelector("#overlay").style.display = "none";
+}
+
+goTown();
